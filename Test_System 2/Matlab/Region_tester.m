041aec6b31d45ef91e7e6505b0e90264b13a943d
Frelsern <mortensletta@hotmail.com>
2013-06-06 10:16:19 +0200

    made matlab folder

---
+++ Test_System 2/Matlab/Region_tester.m
@@ -0,0 +1,14 @@
+clc
+clear all
+close all
+
+
+% reading in filw with region sizes
+text_Regions = fileread('region_sizes.txt');
+Regions = str2num(text_Regions);
+
+[histogram,x] = Region_Histogram_Maker(Regions);
+figure();
+x = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,x];
+histogram = [0;0;0;0;0;0;0;0;0;0;histogram];
+plot(x,histogram);
